a859463290be276e01e37fd1fba2197571cfebd7
alex <nuvorish81@mail.ru>
2021-01-23 11:43:55 +0600

    "Список пользователей"страница сделанна главной, пока других страниц нет.

--- source/main/urls.py
+++ source/main/urls.py
@@ -19,7 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from accounts.views import AllUserView
+
 urlpatterns = [
+    path('', AllUserView.as_view(), name='users'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
